ea48964652afd4022c47d495b655b89c1a71bac1
iLnar <ilnv@mail.ru>
2021-12-01 19:29:29 +0300

    Init commit

---
+++ lib/extender.dart
@@ -0,0 +1,5 @@
+
+library extender;
+
+export 'src/extender_base.dart';
+export 'src/rational_ext.dart';
---
+++ lib/src/rational_ext.dart
@@ -0,0 +1,12 @@
+
+import 'package:rational/rational.dart';
+
+
+Rational rationalInfinity() => Rational.fromInt(9223372036854775807);
+Rational rationalNegativeInfinity() => Rational.fromInt(-9223372036854775808);
+
+///
+/// Rational extension
+///
+extension RationalExt on Rational {
+}
