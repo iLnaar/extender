932bf24a9e28318ecd34ccba44d6b108f2155d22
iLnar <ilnv@mail.ru>
2021-12-12 22:33:32 +0300

    debugMode

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,12 +1,28 @@
 
 enum RoundMode { floor, round }
 
+final debugMode = _debugMode();
 
 const maxBigInt = 9223372036854775807;
 const minBigInt = -9223372036854775808;
 const maxBigIntDigits = 19;
 
 
+///
+///
+///
+bool _debugMode() {
+  bool debug = false;
+  assert(() {
+    debug = true;
+    return true;
+  }());
+  return debug;
+}
+
+///
+///
+///
 String ofName(String? name) =>
     name == null || name == ''
         ? ' '
