cfe93bb538143ae57bc678caba6ce75976e8fda4
iLnar <ilnv@mail.ru>
2022-01-28 20:44:46 +0300

    List.toRandomList()

--- lib/src/list.dart
+++ lib/src/list.dart
@@ -1,5 +1,7 @@
 
 import 'package:quiver/iterables.dart' as quiver;
+import 'int.dart';
+
 
 extension ListExt<E> on List<E> {
   ///
@@ -20,4 +22,16 @@ extension ListExt<E> on List<E> {
   ///
   ///
   E? max([int Function(E, E)? comparator]) => quiver.max(this, comparator);
+
+  ///
+  /// Generates a new list of elements in random order
+  ///
+  List<E> toRandomList() {
+    final randomIndexes = randomIntList(length, false);
+    final randomList = <E>[];
+    for (var index in randomIndexes) {
+      randomList.add(this[index]);
+    }
+    return randomList;
+  }
 }
